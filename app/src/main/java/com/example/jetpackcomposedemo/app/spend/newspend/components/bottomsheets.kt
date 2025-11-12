e142fb9c719e94c60e1b855da255bdf28f925d08
CuongHQ <cuonghq@smartsi.com>
2025-11-13 00:36:53 +0700

    update

--- app/src/main/java/com/example/jetpackcomposedemo/app/spend/newspend/components/bottomsheets.kt
+++ app/src/main/java/com/example/jetpackcomposedemo/app/spend/newspend/components/bottomsheets.kt
@@ -1,18 +1,44 @@
 package com.example.jetpackcomposedemo.app.spend.newspend.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SecondaryTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposedemo.ui.theme.AppColor
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CategoryBottomSheet(showBottomSheet: Boolean, onHideBottomSheet: () -> Unit) {
     val sheetState = rememberModalBottomSheetState()
+    var pagerState = rememberPagerState {
+        2
+    }
+    val scope = rememberCoroutineScope()
+
     if (showBottomSheet) {
         ModalBottomSheet(
             onDismissRequest = {
@@ -21,7 +47,58 @@ fun CategoryBottomSheet(showBottomSheet: Boolean, onHideBottomSheet: () -> Unit)
             sheetState = sheetState,
             contentColor = AppColor.Blue900
         ) {
-
+            SecondaryTabRow(selectedTabIndex = pagerState.currentPage) {
+                Tab(
+                    selected = pagerState.currentPage == 0,
+                    onClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(0)
+                        }
+                    },
+                    text = {
+                        Text(
+                            text = "Category",
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                )
+                Tab(
+                    selected = pagerState.currentPage == 1,
+                    onClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(1)
+                        }
+                    },
+                    text = {
+                        Text(
+                            text = "Detail",
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                )
+            }
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(400.dp)
+            ) { index ->
+                if (index == 0) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(AppColor.Blue900)
+                    )
+                } else {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(AppColor.Green400)
+                    )
+                }
+            }
         }
     }
 }
